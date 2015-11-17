fc68261351d0b54f36cb9ecca4d9d52cf2d7afce
Cristian Carlesso <kentaromiura@gmail.com>
2015-11-17 23:21:05 +0000

    enabling Scan like functionality, multiple bar colors
    Bar now can be configured to show only after an ability connected (eg
    Scan),
    the colour of the bar is configurable and there are 3 states, which can
    have different colours

--- Plugins/Battle/EnemyStats.js
+++ Plugins/Battle/EnemyStats.js
@@ -1,21 +1,58 @@
 /*:
  * @plugindesc Display HP of enemies
  * @author Cristian Carlesso <kentaromiura>
+ * @param showHP
+ * @desc if true show the HP
+ * @default true
  * @param showHPBar
  * @desc if true shows the hp bar
  * @default true
+ * @param padding
+ * @desc the distance between the sprite and the bar
+ * @default 30
+ * @param ===================
  * @param animate
  * @desc if true animates the hp bar
  * @default true
  * @param maxAnimationHeight
  * @desc the amount of pixel the bar will move upwards
  * @default 40
- * @param showHP
- * @desc if true show the HP
- * @default true
- * @param padding
- * @desc the distance between the sprite and the bar
- * @default 20
+ * @param ===================
+ * @param displayAfterAbility
+ * @desc if true will only display after a particular ability succeed
+ * @default false
+ * @param abilityName
+ * @desc the name of the ability we look for
+ * @default Scan
+ * @param ===================
+ * @param backgroundHPBar
+ * @desc the backgroundColor of the enemy HP Bar
+ * @default rgba(255,255,255,0.3)
+ * @param normalHPColorStart
+ * @desc the start gradient color of the normal enemy HP Bar
+ * @default rgba(0,255,0,1)
+ * @param normalHPColorEnd
+ * @desc the end gradient color of the normal enemy HP Bar
+ * @default rgba(0,255,0,0.3)
+ * @param warnThreshold
+ * @desc the warn % threshold
+ * @default 55
+ * @param warnHPColorStart
+ * @desc the start gradient color of the critical enemy HP Bar
+ * @default rgba(255,180,0,1)
+ * @param warnHPColorEnd
+ * @desc the end gradient color of the critical enemy HP Bar
+ * @default rgba(255,180,0,0.3)
+ * @param criticalThreshold
+ * @desc the critical % threshold
+ * @default 30
+ * @param criticalHPColorStart
+ * @desc the start gradient color of the critical enemy HP Bar
+ * @default rgba(255,0,0,1)
+ * @param criticalHPColorEnd
+ * @desc the end gradient color of the critical enemy HP Bar
+ * @default rgba(255,0,0,0.3)
+ *
  */
 
 var dispatchWhenDone = require('../../Common/dispatchWhenDone')
@@ -23,6 +60,41 @@ var animation = require('../../Common/kamicane.cubicBezier')
 var Transition = require('../../Common/transition')
 var getParameters = require('../../Common/getParameters')
 var PARAMETERS = getParameters()
+var alwaysDisplay = PARAMETERS.displayAfterAbility === 'false'
+
+function setAsKnown(id){
+  $dataEnemies[id]._isKnown = true
+}
+
+if(!alwaysDisplay){
+  Game_Action.prototype.apply = dispatchWhenDone(Game_Action.prototype.apply, 'gameActionApplied')
+
+  global.addEventListener('gameActionApplied', function(event){
+    if(event.detail.context.item().name === PARAMETERS.abilityName){
+      var target = event.detail.args[0]
+      if(target.result().isHit()) setAsKnown(target._enemyId)
+    }
+  })
+  // implement save
+  DataManager.makeSaveContents = function(_){
+    return function(){
+      var contents = _()
+      contents.knownEnemies = $dataEnemies.filter(function(enemy){
+        return enemy && enemy._isKnown
+      }).map(function(enemy){
+        return enemy.id
+      })
+      return contents
+    }
+  }(DataManager.makeSaveContents)
+
+  // implement load
+  DataManager.extractSaveContents = dispatchWhenDone(DataManager.extractSaveContents, 'gameLoad')
+  global.addEventListener('gameLoad', function(event){
+    event.detail.args[0].knownEnemies.forEach(setAsKnown)
+    makeCurrentGameSystemNotify()
+  })
+}
 
 function getEnemySprite(enemy){
   return SceneManager._scene._spriteset._battleField.children.filter(function(sprite){
@@ -30,14 +102,16 @@ function getEnemySprite(enemy){
   })[0]
 }
 
-function addBattleEvents(){
-  BattleManager.processDefeat = dispatchWhenDone(BattleManager.processDefeat, 'afterDefeat')
-
+function makeCurrentGameSystemNotify(){
   var events = ['battleStarted', 'battleWon', 'battleEscaped']
-  
+
   ;['onBattleStart', 'onBattleWin', 'onBattleEscape'].forEach(function(event, i){
     $gameSystem[event] = dispatchWhenDone($gameSystem[event], events[i])
   })
+}
+function addBattleEvents(){
+  BattleManager.processDefeat = dispatchWhenDone(BattleManager.processDefeat, 'afterDefeat')
+  makeCurrentGameSystemNotify()
 }
 
 // all the $* variable get set after DataManager.createGameObjects, the plugin though runs before that call.
@@ -63,61 +137,76 @@ var animationFormula = animation([
   transition = new Transition(2000, animationFormula, onTransition),
   onGameObjectsCreated = function(){
     addBattleEvents()
-    
+
     if (PARAMETERS.animate === 'true') {
       transition.loop()
     }
-    
+
     var inBattle = false,
         updateEnemyHP = function(){
           $gameTroop.members().forEach(function(enemy){
-  
+            var dataEnemy = $dataEnemies[enemy._enemyId]
+            var shouldDisplay = alwaysDisplay || dataEnemy._isKnown
             enemy._height = enemy._height || (enemy._height = getEnemySprite(enemy).height)
-    
+
             var hpSprite = enemy._hpSprite || (enemy._hpSprite = new Sprite_Base())
             var hpBar = enemy._hpBar || (enemy._hpBar = new Sprite_Base())
             var hp = '' + enemy._hp
-            var maxHP = enemy._maxHP || (enemy._maxHP = $dataEnemies[enemy._enemyId].params[0])
-    
+            var maxHP = enemy._maxHP || (enemy._maxHP = dataEnemy.params[0])
+
             var textWidth = hp.length * FONT_SIZE
-    
+
             hpSprite.bitmap = new Bitmap(textWidth, FONT_SIZE)
             hpSprite.bitmap.fontSize = FONT_SIZE
             var textHeight = FONT_SIZE
-    
+
             var realTextWidth = hpSprite.bitmap.measureTextWidth(hp)
-            hpSprite.bitmap.drawText(hp, 0, 0, textWidth, FONT_SIZE, 'left')  
+            hpSprite.bitmap.drawText(hp, 0, 0, textWidth, FONT_SIZE, 'left')
             hpSprite.x = enemy.screenX() - realTextWidth/2
-    
+
             var currentY = Math.max(
               enemy.screenY() - (enemy._height + textHeight + PADDING + deltaY),
               textHeight + PADDING + MAX_DELTA - deltaY
             )
-    
+
             hpSprite.y = currentY
             hpBar.bitmap = new Bitmap(100, 10)
-            hpBar.bitmap.fillAll('rgba(0,0,0,0.3)')
+            hpBar.bitmap.fillAll(PARAMETERS.backgroundHPBar)
+            var percent = 100 * enemy._hp/maxHP,
+                colorStart =  PARAMETERS.normalHPColorStart,
+                colorEnd =  PARAMETERS.normalHPColorEnd
+
+            if (percent < PARAMETERS.warnThreshold){
+              colorStart = PARAMETERS.warnHPColorStart
+              colorEnd = PARAMETERS.warnHPColorEnd
+            }
+
+            if (percent < PARAMETERS.criticalThreshold){
+              colorStart = PARAMETERS.criticalHPColorStart
+              colorEnd = PARAMETERS.criticalHPColorEnd
+            }
+
             hpBar.bitmap.gradientFillRect(
               0,
               0,
-              100 * enemy._hp/maxHP,
+              percent,
               10,
-              'rgba(0,255,0,0.3)',
-              'rgba(0,100,0,0.3)'
+              colorStart,
+              colorEnd
             )
-    
+
             hpBar.x = enemy.screenX() - 50
             hpBar.y = currentY + textHeight
-    
+
             if (hp === '0') {
               hpSprite.hide()
               hpBar.hide()
             }
-    
-            if (PARAMETERS.showHPBar === 'true') SceneManager._scene.addChild(hpBar)
-            if (PARAMETERS.showHP === 'true') SceneManager._scene.addChild(hpSprite)
+
+            if (shouldDisplay && PARAMETERS.showHPBar === 'true') SceneManager._scene.addChild(hpBar)
+            if (shouldDisplay && PARAMETERS.showHP === 'true') SceneManager._scene.addChild(hpSprite)
           })
-  
+
         if (inBattle) requestAnimationFrame(updateEnemyHP)
       },
       setBattleOn = function(){
@@ -127,15 +216,12 @@ var animationFormula = animation([
       setBattleOff = function(){
         inBattle = false
       }
-  
+
     global.addEventListener('battleStarted', setBattleOn)
-    
+
     ;['battleWon', 'battleEscaped',  'afterDefeat'].forEach(function(event){
       global.addEventListener(event, setBattleOff)
     })
-  }
+  };
 
 global.addEventListener('gameObjectsCreated', onGameObjectsCreated)
-
-// TODO: add customizable colors
-// TODO: make it show via a `known` flag (scan-like functionality)
--- dist/js/plugins/kentaromiura_Battle_EnemyStats.js
+++ dist/js/plugins/kentaromiura_Battle_EnemyStats.js
@@ -1,20 +1,57 @@
 /*:
  * @plugindesc Display HP of enemies
  * @author Cristian Carlesso <kentaromiura>
+ * @param showHP
+ * @desc if true show the HP
+ * @default true
  * @param showHPBar
  * @desc if true shows the hp bar
  * @default true
+ * @param padding
+ * @desc the distance between the sprite and the bar
+ * @default 30
+ * @param ===================
  * @param animate
  * @desc if true animates the hp bar
  * @default true
  * @param maxAnimationHeight
  * @desc the amount of pixel the bar will move upwards
  * @default 40
- * @param showHP
- * @desc if true show the HP
- * @default true
- * @param padding
- * @desc the distance between the sprite and the bar
- * @default 20
+ * @param ===================
+ * @param displayAfterAbility
+ * @desc if true will only display after a particular ability succeed
+ * @default false
+ * @param abilityName
+ * @desc the name of the ability we look for
+ * @default Scan
+ * @param ===================
+ * @param backgroundHPBar
+ * @desc the backgroundColor of the enemy HP Bar
+ * @default rgba(255,255,255,0.3)
+ * @param normalHPColorStart
+ * @desc the start gradient color of the normal enemy HP Bar
+ * @default rgba(0,255,0,1)
+ * @param normalHPColorEnd
+ * @desc the end gradient color of the normal enemy HP Bar
+ * @default rgba(0,255,0,0.3)
+ * @param warnThreshold
+ * @desc the warn % threshold
+ * @default 55
+ * @param warnHPColorStart
+ * @desc the start gradient color of the critical enemy HP Bar
+ * @default rgba(255,180,0,1)
+ * @param warnHPColorEnd
+ * @desc the end gradient color of the critical enemy HP Bar
+ * @default rgba(255,180,0,0.3)
+ * @param criticalThreshold
+ * @desc the critical % threshold
+ * @default 30
+ * @param criticalHPColorStart
+ * @desc the start gradient color of the critical enemy HP Bar
+ * @default rgba(255,0,0,1)
+ * @param criticalHPColorEnd
+ * @desc the end gradient color of the critical enemy HP Bar
+ * @default rgba(255,0,0,0.3)
+ * 
  */
-(function(c,d){var a={},b=function(f){var e=a[f];if(!e){e=a[f]={};var g=e.exports={};c[f].call(g,b,e,g,d)}return e.exports};b('0')}({'0':function(d,s,r,g){var c=d('1');var l=d('2');var m=d('3');var o=d('4');var a=o();function q(a){return SceneManager._scene._spriteset._battleField.children.filter(function(b){return b._enemy===a})[0]}function p(){BattleManager.processDefeat=c(BattleManager.processDefeat,'afterDefeat');var a=['battleStarted','battleWon','battleEscaped'];;['onBattleStart','onBattleWin','onBattleEscape'].forEach(function(b,d){$gameSystem[b]=c($gameSystem[b],a[d])})}DataManager.createGameObjects=c(DataManager.createGameObjects,'gameObjectsCreated');var k=l([{x:0,y:.89},{x:.15,y:-.3},{x:.25,y:.02},{x:.52,y:.26},{x:.8,y:.02},{x:.85,y:-.31},{x:1,y:.88}],1e-4),e=0,h=a.maxAnimationHeight-0,b=32,f=a.padding-0,j=function(a){e=h*a},n=new m(2e3,k,j),i=function(){p();if(a.animate==='true'){n.loop()}var c=false,d=function(){$gameTroop.members().forEach(function(c){c._height=c._height||(c._height=q(c).height);var d=c._hpSprite||(c._hpSprite=new Sprite_Base);var g=c._hpBar||(c._hpBar=new Sprite_Base);var i=''+c._hp;var m=c._maxHP||(c._maxHP=$dataEnemies[c._enemyId].params[0]);var k=i.length*b;d.bitmap=new Bitmap(k,b);d.bitmap.fontSize=b;var j=b;var n=d.bitmap.measureTextWidth(i);d.bitmap.drawText(i,0,0,k,b,'left');d.x=c.screenX()-n/2;var l=Math.max(c.screenY()-(c._height+j+f+e),j+f+h-e);d.y=l;g.bitmap=new Bitmap(100,10);g.bitmap.fillAll('rgba(0,0,0,0.3)');g.bitmap.gradientFillRect(0,0,100*c._hp/m,10,'rgba(0,255,0,0.3)','rgba(0,100,0,0.3)');g.x=c.screenX()-50;g.y=l+j;if(i==='0'){d.hide();g.hide()}if(a.showHPBar==='true')SceneManager._scene.addChild(g);if(a.showHP==='true')SceneManager._scene.addChild(d)});if(c)requestAnimationFrame(d)},i=function(){c=true;requestAnimationFrame(d)},j=function(){c=false};g.addEventListener('battleStarted',i);;['battleWon','battleEscaped','afterDefeat'].forEach(function(a){g.addEventListener(a,j)})};g.addEventListener('gameObjectsCreated',i)},'1':function(b,c,e,d){var a=b('5');c.exports=function b(b,c){return function(){b.apply(this,a(arguments));d.dispatchEvent(new CustomEvent(c))}}},'2':function(e,d,f,g){var a=function(g,j,i,h,a){var b=1-a,e=a*a*a,d=3*a*a*b,c=3*a*b*b,f=b*b*b;return h*e+i*d+j*c+g*f};var b=function(a,b,c){if(a<b)return b;if(a>c)return c;return a};var c=function(c,i){if(c.length%3!==1)throw new Error('invalid input');for(var d=0;d<c.length-1;d+=3){var e=c[d],g=c[d+1],h=c[d+2],f=c[d+3];if(d===0)e.x=0;else e.x=b(e.x,0,1);if(d===c.length-4)f.x=1;else f.x=b(f.x,e.x,1);g.x=b(g.x,e.x,f.x);h.x=b(h.x,e.x,f.x)}return function(e){var d,j,k,b;for(var f=0;f<c.length-1;f+=3){d=c[f];j=c[f+1];k=c[f+2];b=c[f+3];if(e>=d.x&&e<=b.x)break}var g=0,l=1,h=e,m;if(e<g)return a(d.y,j.y,k.y,b.y,g);if(e>l)return a(d.y,j.y,k.y,b.y,l);while(g<l){m=a(d.x,j.x,k.x,b.x,h);if(Math.abs(m-e)<i)return a(d.y,j.y,k.y,b.y,h);if(e>m)g=h;else l=h;h=(l-g)*.5+g}return a(d.y,j.y,k.y,b.y,h)}};d.exports=c},'3':function(d,e,f,g){var b=d('6');function c(a,b,c,d){if(!a)throw new Error('no duration given');if(!b)throw new Error('no equation given');if(!c)throw new Error('no onStep given');this.duration=a;this.equation=b;this.onStep=c;this.onEnd=d||function(){};this.step=this.step.bind(this)}function a(a){b(a);return function(){this.elapsed=this.duration}}c.prototype={get paused(){return this.cancel==null&&this.elapsed!=null},get active(){return this.cancel!=null},get idle(){return this.cancel==null&&this.elapsed==null},start:function(){if(this.idle){this.elapsed=0;this.cancel=a(this.step)}return this},step:function(c){this.elapsed+=c-(this.time||c);var b=this.elapsed/this.duration;if(b>1)b=1;if(b!==1){this.time=c;this.cancel=a(this.step)}else{this.cancel=this.time=this.elapsed=null;a(this.onEnd)}var d=this.equation(b);this.onStep(d)},stop:function(){if(this.active){this.cancel();this.elapsed=this.cancel=this.time=null}return this},pause:function(){if(this.active){this.cancel();this.cancel=this.time=null}return this},loop:function(){var a=this;a.onEnd=function(){a.start()};a.start();return{cancel:function(){a.onEnd=function(){};a.cancel()}}},resume:function(){if(this.paused){this.cancel=a(this.step)}return this}};e.exports=c},'4':function(b,c,d,e){var a=b('7');c.exports=function(){return PluginManager.parameters(a())}},'5':function(b,a,c,d){a.exports=Function.prototype.call.bind(Array.prototype.slice)},'6':function(c,b,d,a){b.exports=a.requestAnimationFrame||a.webkitRequestAnimationFrame||a.mozRequestAnimationFrame||a.oRequestAnimationFrame||a.msRequestAnimationFrame||function(a){setTimeout(function(){a(+new Date)},1e3/60)}},'7':function(b,a,c,d){a.exports=function(){return/([^\/]+)\.js$/.exec(document.currentScript.src)[1]}}},this))
+(function(c,d){var a={},b=function(f){var e=a[f];if(!e){e=a[f]={};var g=e.exports={};c[f].call(g,b,e,g,d)}return e.exports};b('0')}({'0':function(e,v,u,d){var b=e('1');var m=e('2');var o=e('3');var q=e('4');var a=q();var g=a.displayAfterAbility==='false';function j(a){$dataEnemies[a]._isKnown=true}if(!g){Game_Action.prototype.apply=b(Game_Action.prototype.apply,'gameActionApplied');d.addEventListener('gameActionApplied',function(c){if(c.detail.context.item().name===a.abilityName){var b=c.detail.args[0];if(b.result().isHit())j(b._enemyId)}});DataManager.makeSaveContents=function(a){return function(){var b=a();b.knownEnemies=$dataEnemies.filter(function(a){return a&&a._isKnown}).map(function(a){return a.id});return b}}(DataManager.makeSaveContents);DataManager.extractSaveContents=b(DataManager.extractSaveContents,'gameLoad');d.addEventListener('gameLoad',function(a){a.detail.args[0].knownEnemies.forEach(j);n()})}function t(a){return SceneManager._scene._spriteset._battleField.children.filter(function(b){return b._enemy===a})[0]}function n(){var a=['battleStarted','battleWon','battleEscaped'];;['onBattleStart','onBattleWin','onBattleEscape'].forEach(function(c,d){$gameSystem[c]=b($gameSystem[c],a[d])})}function s(){BattleManager.processDefeat=b(BattleManager.processDefeat,'afterDefeat');n()}DataManager.createGameObjects=b(DataManager.createGameObjects,'gameObjectsCreated');var p=m([{x:0,y:.89},{x:.15,y:-.3},{x:.25,y:.02},{x:.52,y:.26},{x:.8,y:.02},{x:.85,y:-.31},{x:1,y:.88}],1e-4),f=0,i=a.maxAnimationHeight-0,c=32,h=a.padding-0,r=function(a){f=i*a},l=new o(2e3,p,r),k=function(){s();if(a.animate==='true'){l.loop()}var b=false,e=function(){$gameTroop.members().forEach(function(b){var p=$dataEnemies[b._enemyId];var r=g||p._isKnown;b._height=b._height||(b._height=t(b).height);var d=b._hpSprite||(b._hpSprite=new Sprite_Base);var e=b._hpBar||(b._hpBar=new Sprite_Base);var j=''+b._hp;var u=b._maxHP||(b._maxHP=p.params[0]);var o=j.length*c;d.bitmap=new Bitmap(o,c);d.bitmap.fontSize=c;var m=c;var s=d.bitmap.measureTextWidth(j);d.bitmap.drawText(j,0,0,o,c,'left');d.x=b.screenX()-s/2;var q=Math.max(b.screenY()-(b._height+m+h+f),m+h+i-f);d.y=q;e.bitmap=new Bitmap(100,10);e.bitmap.fillAll(a.backgroundHPBar);var k=100*b._hp/u,n=a.normalHPColorStart,l=a.normalHPColorEnd;if(k<a.warnThreshold){n=a.warnHPColorStart;l=a.warnHPColorEnd}if(k<a.criticalThreshold){n=a.criticalHPColorStart;l=a.criticalHPColorEnd}e.bitmap.gradientFillRect(0,0,k,10,n,l);e.x=b.screenX()-50;e.y=q+m;if(j==='0'){d.hide();e.hide()}if(r&&a.showHPBar==='true')SceneManager._scene.addChild(e);if(r&&a.showHP==='true')SceneManager._scene.addChild(d)});if(b)requestAnimationFrame(e)},j=function(){b=true;requestAnimationFrame(e)},k=function(){b=false};d.addEventListener('battleStarted',j);;['battleWon','battleEscaped','afterDefeat'].forEach(function(a){d.addEventListener(a,k)})};d.addEventListener('gameObjectsCreated',k)},'1':function(b,c,e,d){var a=b('5');c.exports=function b(b,c){return function(){var e=a(arguments);var f=b.apply(this,e);d.dispatchEvent(new CustomEvent(c,{'detail':{args:e,result:f,context:this}}));return f}}},'2':function(e,d,f,g){var a=function(g,j,i,h,a){var b=1-a,e=a*a*a,d=3*a*a*b,c=3*a*b*b,f=b*b*b;return h*e+i*d+j*c+g*f};var b=function(a,b,c){if(a<b)return b;if(a>c)return c;return a};var c=function(c,i){if(c.length%3!==1)throw new Error('invalid input');for(var d=0;d<c.length-1;d+=3){var e=c[d],g=c[d+1],h=c[d+2],f=c[d+3];if(d===0)e.x=0;else e.x=b(e.x,0,1);if(d===c.length-4)f.x=1;else f.x=b(f.x,e.x,1);g.x=b(g.x,e.x,f.x);h.x=b(h.x,e.x,f.x)}return function(e){var d,j,k,b;for(var f=0;f<c.length-1;f+=3){d=c[f];j=c[f+1];k=c[f+2];b=c[f+3];if(e>=d.x&&e<=b.x)break}var g=0,l=1,h=e,m;if(e<g)return a(d.y,j.y,k.y,b.y,g);if(e>l)return a(d.y,j.y,k.y,b.y,l);while(g<l){m=a(d.x,j.x,k.x,b.x,h);if(Math.abs(m-e)<i)return a(d.y,j.y,k.y,b.y,h);if(e>m)g=h;else l=h;h=(l-g)*.5+g}return a(d.y,j.y,k.y,b.y,h)}};d.exports=c},'3':function(d,e,f,g){var b=d('7');function c(a,b,c,d){if(!a)throw new Error('no duration given');if(!b)throw new Error('no equation given');if(!c)throw new Error('no onStep given');this.duration=a;this.equation=b;this.onStep=c;this.onEnd=d||function(){};this.step=this.step.bind(this)}function a(a){b(a);return function(){this.elapsed=this.duration}}c.prototype={get paused(){return this.cancel==null&&this.elapsed!=null},get active(){return this.cancel!=null},get idle(){return this.cancel==null&&this.elapsed==null},start:function(){if(this.idle){this.elapsed=0;this.cancel=a(this.step)}return this},step:function(c){this.elapsed+=c-(this.time||c);var b=this.elapsed/this.duration;if(b>1)b=1;if(b!==1){this.time=c;this.cancel=a(this.step)}else{this.cancel=this.time=this.elapsed=null;a(this.onEnd)}var d=this.equation(b);this.onStep(d)},stop:function(){if(this.active){this.cancel();this.elapsed=this.cancel=this.time=null}return this},pause:function(){if(this.active){this.cancel();this.cancel=this.time=null}return this},loop:function(){var a=this;a.onEnd=function(){a.start()};a.start();return{cancel:function(){a.onEnd=function(){};a.cancel()}}},resume:function(){if(this.paused){this.cancel=a(this.step)}return this}};e.exports=c},'4':function(b,c,d,e){var a=b('6');c.exports=function(){return PluginManager.parameters(a())}},'5':function(b,a,c,d){a.exports=Function.prototype.call.bind(Array.prototype.slice)},'6':function(b,a,c,d){a.exports=function(){return/([^\/]+)\.js$/.exec(document.currentScript.src)[1]}},'7':function(c,b,d,a){b.exports=a.requestAnimationFrame||a.webkitRequestAnimationFrame||a.mozRequestAnimationFrame||a.oRequestAnimationFrame||a.msRequestAnimationFrame||function(a){setTimeout(function(){a(+new Date)},1e3/60)}}},this))
