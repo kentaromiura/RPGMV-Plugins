f6e3430ccd99df19bf1ab39a3bbf903b0f19eee0
Cristian Carlesso <kentaromiura@gmail.com>
2015-11-22 00:00:27 +0000

    Enable copy and paste on OSX debug console

--- Plugins/Generic/Debug.js
+++ Plugins/Generic/Debug.js
@@ -4,8 +4,24 @@
  * @param debug
  * @desc if true opens chrome inspector for testing
  * @default false
+ * @param isOSX
+ * @default false
+ * @desc if true enable copy/paste for OSX
  */
 
 var getParameters = require('../../Common/getParameters')
+var gui = require('nw.gui');
+if (getParameters().debug === 'true') gui.Window.get().showDevTools()
 
-if (getParameters().debug === 'true') require('nw.gui').Window.get().showDevTools()
+global.addEventListener('keydown', function(event) {
+  if(event.ctrlKey && event.keyCode == 82) {
+    location.reload();
+    event.preventDefault();
+  }
+});
+
+if (getParameters().isOSX === 'true') {
+  var mb = new gui.Menu({type:"menubar"});
+  mb.createMacBuiltin("DEBUG");
+  gui.Window.get().menu = mb;
+}
--- dist/js/plugins/kentaromiura_Generic_Debug.js
+++ dist/js/plugins/kentaromiura_Generic_Debug.js
@@ -4,5 +4,8 @@
  * @param debug
  * @desc if true opens chrome inspector for testing
  * @default false
+ * @param isOSX
+ * @default false
+ * @desc if true enable copy/paste for OSX
  */
-(function(c,d){var a={},b=function(f){var e=a[f];if(!e){e=a[f]={};var g=e.exports={};c[f].call(g,b,e,g,d)}return e.exports};b('0')}({'0':function(b,c,d,e){var a=b('1');if(a().debug==='true')require('nw.gui').Window.get().showDevTools()},'1':function(b,c,d,e){var a=b('2');c.exports=function(){return PluginManager.parameters(a())}},'2':function(b,a,c,d){a.exports=function(){return/([^\/]+)\.js$/.exec(document.currentScript.src)[1]}}},this))
+(function(c,d){var a={},b=function(f){var e=a[f];if(!e){e=a[f]={};var g=e.exports={};c[f].call(g,b,e,g,d)}return e.exports};b('0')}({'0':function(d,f,g,e){var b=d('1');var a=require('nw.gui');if(b().debug==='true')a.Window.get().showDevTools();e.addEventListener('keydown',function(a){if(a.ctrlKey&&a.keyCode==82){location.reload();a.preventDefault()}});if(b().isOSX==='true'){var c=new a.Menu({type:'menubar'});c.createMacBuiltin('DEBUG');a.Window.get().menu=c}},'1':function(b,c,d,e){var a=b('2');c.exports=function(){return PluginManager.parameters(a())}},'2':function(b,a,c,d){a.exports=function(){return/([^\/]+)\.js$/.exec(document.currentScript.src)[1]}}},this))
